f357e2752688da6561d0e28b810c94c7f1dfc344
MuhammadMoustafa <muhammad.moustafa@intixel.com>
2022-10-04 14:37:13 +0200

    Full screen

--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,33 @@
-let drop_array = []; 
+let drop_array = [];
 const DROP_NUMBER = 500;
 let slider;
 
 function setup() {
-    createCanvas(800, 800);
-    for(let i=0; i<DROP_NUMBER; i++){
-        drop_array.push(new Drop(width/2, 0, 1));
-    }
-    d = new Drop(width/2, 0, 1);
-
-    slider = createSlider(0, 255, 100);
-    slider.position(10, 10);
-    slider.style('width', '80px');
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < DROP_NUMBER; i++) {
+    drop_array.push(new Drop());
+  }
+  slider = createSlider(0, 255, 100);
+  slider.position(10, 10);
+  slider.style("width", "80px");
 }
 
 function draw() {
-    background(230, 230, 230);
-    for(let i=0; i<DROP_NUMBER; i++){
-        drop_array[i].fall();
-        drop_array[i].show();
-    }
+  background(230, 230, 230);
+  for (let i = 0; i < DROP_NUMBER; i++) {
+    drop_array[i].fall();
+    drop_array[i].show();
   }
+}
+
+/* full screening will change the size of the canvas */
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+/* prevents the mobile browser from processing some default
+ * touch events, like swiping left for "back" or scrolling the page.
+ */
+document.ontouchmove = function(event) {
+    event.preventDefault();
+};
